57a540860f7068fcf2e29611e445e0e3679e0276
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-02-07 18:26:10 +0300

    Make App a function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,13 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import './style.scss';
 import SearchBar from '../SearchBar/SearchBar';
 
-class App extends PureComponent {
-  render() {
-    return (
-      <div>
-        <SearchBar />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 export default App;
