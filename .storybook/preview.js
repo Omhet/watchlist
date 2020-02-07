9c18007e4eec7fc999cf9f86f377d4f7cdd7aed2
Vladimir Ivanov <vlivanov@aligntech.com>
2020-02-07 18:24:58 +0300

    Center stories in container

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
 import '../src/index.scss';
+
+const storyStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: 911
+};
+
+addDecorator(storyFn => <div style={storyStyle}>{storyFn()}</div>);
