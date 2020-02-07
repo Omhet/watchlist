41c43e3c0a94f252494dc8db8cfb03889f2e1a8d
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-02-07 15:16:38 +0300

    Init webpack/react/ts

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from "react";
+import "./style.css";
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
