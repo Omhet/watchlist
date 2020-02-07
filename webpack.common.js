69a492147d99c24a8f0301ac8184f4138ea13e1d
Vladimir Ivanov <vlivanov@aligntech.com>
2020-02-07 15:16:38 +0300

    Init webpack/react/ts

---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  stats: {
+    children: false,
+    builtAt: false,
+    assetsSort: "!size",
+    entrypoints: false,
+    hash: false,
+    modules: false,
+    version: false,
+    timings: false
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(js|ts)x?$/,
+        use: ["babel-loader"],
+        include: path.resolve(__dirname, "src")
+      }
+    ]
+  }
+};
