72091a9dc7a865c3c74837a3c7667a84b230c182
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-02-07 17:46:38 +0300

    Add global sass colors. Use webpack app config in Storybook

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react';
 import './style.scss';
+import SearchBar from '../SearchBar/SearchBar';
 
 class App extends PureComponent {
   render() {
     return (
       <div>
         <h1>Hello</h1>
+        <SearchBar />
       </div>
     );
   }
