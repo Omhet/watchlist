138d42c261ed7d64dd1924db8902c6500f1d0238
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-02-07 16:14:30 +0300

    Add hidden files

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"]
+  },
+  plugins: ["@typescript-eslint", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react"
+  ],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off"
+  }
+};
