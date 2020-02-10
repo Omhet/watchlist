fae27d97bc9bcb8d3f7300bc1f9c4c5fee84ebd9
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-02-10 17:01:06 +0300

    Add Logo. Import both Montserrat and Montserrat Bold

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react';
+import styles from './style.scss';
+
+const Logo: FunctionComponent = () => {
+  return <div className={styles.main}>WannaWatch</div>;
+};
+
+export default Logo;
