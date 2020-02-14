c1f077568f3c4c0415deac960a88c4c29d769760
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-02-14 22:58:21 +0300

    Dropdown functionality

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,21 +1,41 @@
-import React, { FunctionComponent, ReactNode } from 'react';
+import React, { FunctionComponent, ReactNode, useState } from 'react';
 import styles from './style.scss';
 import Button from '../Button/Button';
 import DownArrowIcon from '../../icons/DownArrow.svg';
 
 interface Props {
   header: ReactNode;
+  isOpenByDefault?: boolean;
 }
 
-const Dropdown: FunctionComponent<Props> = ({ header }) => {
+const Dropdown: FunctionComponent<Props> = ({
+  header,
+  children,
+  isOpenByDefault = false
+}) => {
+  const [isOpen, setIsOpen] = useState(isOpenByDefault);
+
+  const handleHeaderClick = () => {
+    setIsOpen(isOpen => !isOpen);
+  };
+
+  const handleItemClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className={styles.main}>
-      <Button>
-        <>
+      <Button onClick={handleHeaderClick}>
+        <div className={styles.header}>
           {header}
           <DownArrowIcon className={styles.arrow} />
-        </>
+        </div>
       </Button>
+      {isOpen && (
+        <div onClick={handleItemClick} className={styles.body}>
+          {children}
+        </div>
+      )}
     </div>
   );
 };
