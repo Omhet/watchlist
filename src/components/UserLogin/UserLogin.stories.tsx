f31ff765248b4f2df5da82b47a550be9ddba522f
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-02-15 12:32:18 +0300

    Add UserLogin

---
+++ src/components/UserLogin/UserLogin.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import UserLogin from './UserLogin';
+
+export default {
+  title: 'UserLogin'
+};
+
+export const NotLoggedIn = () => (
+  <UserLogin onLoginClick={() => console.log('Login')} isUserLoggedIn={false} />
+);
+
+export const LoggedIn = () => (
+  <UserLogin
+    onLogoutClick={() => console.log('Logout')}
+    isUserLoggedIn
+    userName="John Doe"
+  />
+);
