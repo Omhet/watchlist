45808c0150035ca53250402a46e829e2c8721e4b
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-04-30 12:13:43 +0300

    Add MovieList to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './style.scss';
 import PageHeader from '../PageHeader/PageHeader';
+import MovieList from '../MovieList/MovieList';
+import movies from '../../stories/MovieList/data.json';
 
 const App = () => {
   return (
     <div className={styles.main}>
       <PageHeader />
+      <MovieList movies={movies} />
     </div>
   );
 };
