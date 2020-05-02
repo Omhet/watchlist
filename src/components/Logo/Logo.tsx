3051da18875e7f247bbb08773b8f829e07139d34
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-05-02 15:40:05 +0300

    Rework Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,15 @@
 import React, { FunctionComponent } from 'react';
+import LogoIcon from '../../icons/VHS.svg';
 import styles from './style.scss';
+import { isSmallScreen } from '../../utils/device';
 
 const Logo: FunctionComponent = () => {
-  return <div className={styles.main}>WannaWatch</div>;
+  return (
+    <div className={styles.main}>
+      {!isSmallScreen() && <span className={styles.text}>WannaWatch</span>}
+      <LogoIcon width={51} height={32} />
+    </div>
+  );
 };
 
 export default Logo;
--- src/components/Logo/style.scss.d.ts
+++ src/components/Logo/style.scss.d.ts
@@ -1,6 +1,7 @@
 declare namespace StyleScssModule {
   export interface IStyleScss {
     main: string;
+    text: string;
   }
 }
 
