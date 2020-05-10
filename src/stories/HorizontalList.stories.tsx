0930aea7ba782568c920edab27d0a077bc1696f8
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-05-10 13:28:03 +0300

    Tune PersonCard style

--- src/stories/HorizontalList.stories.tsx
+++ src/stories/HorizontalList.stories.tsx
@@ -13,13 +13,24 @@ const itemProps = {
   title: 'Rosie Betzler'
 };
 
+const otherItemProps = {
+  image:
+    'https://image.tmdb.org/t/p/w276_and_h350_face/6NsMbJXRlDZuDzatN2akFdGuTvx.jpg',
+  name: 'Roman Griffin Davis Davis Davis',
+  title: 'Johannes "Jojo" Betzler'
+};
+
+const anotherItemProps = {
+  image:
+    'https://image.tmdb.org/t/p/w276_and_h350_face/6NsMbJXRlDZuDzatN2akFdGuTvx.jpg',
+  name: 'Roman Griffin',
+  title: 'Johannes "Jojo" Betzler'
+};
+
 export const Basic = () => (
   <HorizontalList>
     <PersonCard {...itemProps} />
-    <PersonCard {...itemProps} />
-    <PersonCard {...itemProps} />
-    <PersonCard {...itemProps} />
-    <PersonCard {...itemProps} />
-    <PersonCard {...itemProps} />
+    <PersonCard {...otherItemProps} />
+    <PersonCard {...anotherItemProps} />
   </HorizontalList>
 );
