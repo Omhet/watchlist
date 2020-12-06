ac8de7a55c79a75d7bf3f42433a712a93e961599
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-12-06 13:31:31 +0300

    Use dotenv

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.tsx',
